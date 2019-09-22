e0589c3caa451918f6c15fbf84887062bce735ff
stefanos <satchfast@gmail.com>
2019-09-22 19:27:59 +0300

    minor

--- app/V/header.php
+++ app/V/header.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <style>
@@ -199,5 +200,11 @@ body {
 .btn-facebook {
   color: white;
   background-color: #3b5998;
+}
+
+
+.custom_transform {
+  transform: skew(0, -1.5deg);
+  transform-origin: bottom left;
 }
 </style>
